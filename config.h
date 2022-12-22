532b4f319d7aa0d9124e39ab319b67d2eaedef1b
Michał Szczepaniak <m.szczepaniak.000@gmail.com>
2022-12-22 13:08:52 +0100

    Update info.json by removing duplicates

--- config.h
+++ config.h
@@ -19,26 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-
-/* key matrix size */
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 15
-
-/*
- * Keyboard Matrix Assignments
- *
- * Change this to how you wired your keyboard
- * COLS: AVR pins used for columns, left to right
- * ROWS: AVR pins used for rows, top to bottom
- * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
- *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
- *
- */
-#define MATRIX_ROW_PINS { B12, B13, B14, B15, A8 }
-#define MATRIX_COL_PINS { B3, B4, B5, B6, B7, B8, A3, B1, A7, B0, A6, A4, A5, B10, A15 }
-
-
 #define AUDIO_INIT_DELAY
 #define AUDIO_PIN A2
 #define AUDIO_PWM_DRIVER PWMD2
